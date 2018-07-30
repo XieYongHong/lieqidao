c0ebf1ac3834235a0d1f26b9644ee53a98f9df61
谢永红 <641744504@qq.com>
2018-07-30 20:32:29 +0800

    添加列表详情

---
+++ src/page/wall_Info/info.vue
@@ -0,0 +1,30 @@
+<template>
+    <a>
+        <div class="pic">
+            <img src="">
+        </div>
+        <p class="">
+            {{text}}
+        </p>
+        <p>
+            <span>
+                <i></i>
+                {{eyeNumber}}
+            </span>
+            <span>
+                <i></i>
+                {{houseNumber}}
+            </span>
+        </p>
+    </a>
+</template>
+
+<script>
+export default {
+    props:['text','eyeNumber','houseNumber']
+}
+</script>
+
+<style>
+
+</style>
